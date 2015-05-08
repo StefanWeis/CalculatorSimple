fd80e52d69a9f248a6075fdcad81b8947459816e
stefanweis01 <stefanweis01@gmail.com>
2015-05-08 09:37:33 +0200

    Initial commit

---
+++ app/src/main/java/de/dma/calculatorsimple/MainActivity.java
@@ -0,0 +1,138 @@
+package de.dma.calculatorsimple;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button_c = (Button) findViewById(R.id.button_c);
+
+        button_c.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                TextView exp = (TextView) findViewById(R.id.expression);
+                exp.setText("");
+                return true;
+            }
+        });
+
+        button_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView exp = (TextView) findViewById(R.id.expression);
+                exp.setText(exp.getText().toString().substring(0, exp.getText().length() - 1));
+            }
+        });
+
+        Button button_equals = (Button) findViewById(R.id.button_equals);
+
+        button_equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView expression = (TextView) findViewById(R.id.expression);
+
+                Expression e = new ExpressionBuilder(expression.getText().toString()).build();
+                double result = e.evaluate();
+
+                expression.setText(Double.toString(result));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v ) {
+
+        TextView exp = (TextView) findViewById(R.id.expression);
+
+        switch (v.getId()) {
+            case R.id.button_plusminus:
+                exp.append("");
+                break;
+            case  R.id.button_euro:
+                exp.append("");
+                break;
+            case R.id.button_division:
+                exp.append("/");
+                break;
+            case R.id.button_7:
+                exp.append("7");
+                break;
+            case R.id.button_8:
+                exp.append("8");
+                break;
+            case R.id.button_9:
+                exp.append("9");
+                break;
+            case R.id.button_multiply:
+                exp.append("*");
+                break;
+            case R.id.button_4:
+                exp.append("4");
+                break;
+            case R.id.button_5:
+                exp.append("5");
+                break;
+            case R.id.button_6:
+                exp.append("6");
+                break;
+            case R.id.button_addition:
+                exp.append("+");
+                break;
+            case R.id.button_1:
+                exp.append("1");
+                break;
+            case R.id.button_2:
+                exp.append("2");
+                break;
+            case R.id.button_3:
+                exp.append("3");
+                break;
+            case R.id.button_minus:
+                exp.append("-");
+                break;
+            case R.id.button_0:
+                exp.append("0");
+                break;
+            case R.id.button_dot:
+                exp.append(".");
+                break;
+            default:
+                break;
+        }
+    }
+}
